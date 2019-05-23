67e5e6f1a56f34be7d4caec5409b19af7b94f996
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2019-05-23 17:01:08 -0400

    missing a quote

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.get(`/about`, (req, res) => {
 // Starting our server on port 4000
 
 const port = (process.env.PORT || 4000);
-app.listen(port, () => console.log('Listening on 4000'));
+app.listen(port, () => console.log(`Listening on ${port}`));
