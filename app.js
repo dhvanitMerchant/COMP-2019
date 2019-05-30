68ef2d79839d5602b8f222abea1cb740bf94d805
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2019-05-30 16:05:05 -0400

    Hello Part 02

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // Our imported libraries
 const express = require('express');
+const path = require('path');
+
+
 // Assigning Express to an app constant
 const app = express();
 // Creating our first route which is looking for requests from http://localhost:4000/
-app.get(`/`, (req, res) => { // res is the response object and req is the request object
- // Our response
- res.send(`Hey Party... Kem Che?`);
-});
-// Creating our first route which is looking for requests from http://localhost:4000/
-app.get(`/about`, (req, res) => {
- // Our response
- res.send(`Hey 'dere world!`);
-});
+//OUR routes 
+const routes = require('./routes.js');
+app.use('/', routes);
+
+// Our Views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 // Starting our server on port 4000
 app.listen(4000, () => console.log('Listening on 4000'));
